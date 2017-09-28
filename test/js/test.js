697cef9fcf63d9de34e9362ed5951872b1a6aa3b
Kevin Smith <ksjazzguitar@yahoo.com>
2017-09-27 21:39:38 -0700

    test of AJAX

--- test/js/test.js
+++ test/js/test.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   console.log('in doc ready')
 
-  $.getJSON('./test/data/test.json', function(data) {
+  $.getJSON('./data/test.json', function(data) {
     console.log('in json callback')
     console.log(data)
   })
 
-  $.getJSON('./test/data/test.txt', function(data) {
+  $.getJSON('./data/test.txt', function(data) {
     console.log('in txt callback')
     console.log(data)
   })
