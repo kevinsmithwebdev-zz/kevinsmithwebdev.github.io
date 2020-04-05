6d8d47e5dc06b8cc9ed10d9f9c36cca935b6f6da
Kevin Smith <kevin.smith@jbhunt.com>
2020-04-05 13:32:43 -0500

    lectorem

---
+++ pages/lectorem/precache-manifest.6c9e192e33c40a28ff5e94c2ea0fc09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f539c22cc953c93d2cea3d2ad0464279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15752c93317803897837",
+    "url": "/static/css/main.0ffd2ef4.chunk.css"
+  },
+  {
+    "revision": "21aa4155fe020a58f00a",
+    "url": "/static/js/2.70eee0cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70eee0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15752c93317803897837",
+    "url": "/static/js/main.0d5ff345.chunk.js"
+  },
+  {
+    "revision": "2d4552c7810abad847de",
+    "url": "/static/js/runtime-main.874b1ca8.js"
+  }
+]);
