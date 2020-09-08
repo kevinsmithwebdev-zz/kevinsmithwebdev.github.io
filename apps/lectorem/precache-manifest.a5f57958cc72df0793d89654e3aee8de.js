924397283962b16bc73d66b5ddce03846ee89ded
Kevin Smith <kevin.smith@jbhunt.com>
2020-09-08 14:22:34 -0500

    adding folders

---
+++ apps/lectorem/precache-manifest.a5f57958cc72df0793d89654e3aee8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c4be611d5aa0e1e5bde57524ecc1fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849391f785ea8bc249e6",
+    "url": "/static/css/main.57a64490.chunk.css"
+  },
+  {
+    "revision": "688b784d29d75144a470",
+    "url": "/static/js/2.a6567296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6567296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849391f785ea8bc249e6",
+    "url": "/static/js/main.3140c2d1.chunk.js"
+  },
+  {
+    "revision": "806b4c19baefec61083c",
+    "url": "/static/js/runtime-main.774d35f6.js"
+  }
+]);
