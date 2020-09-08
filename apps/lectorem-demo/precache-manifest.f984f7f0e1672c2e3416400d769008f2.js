11f1a747d38efdac5cc1b2b6fa66f301953fd972
Kevin Smith <kevin.smith@jbhunt.com>
2020-09-08 14:44:15 -0500

    working

---
+++ apps/lectorem-demo/precache-manifest.f984f7f0e1672c2e3416400d769008f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97011f3ae90507a6654121076a3c78b9",
+    "url": "/apps/lectorem-demo/index.html"
+  },
+  {
+    "revision": "a7d60ad9773270b0e275",
+    "url": "/apps/lectorem-demo/static/css/main.57a64490.chunk.css"
+  },
+  {
+    "revision": "1c8edf0b02e2a197fd76",
+    "url": "/apps/lectorem-demo/static/js/2.1ac960b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apps/lectorem-demo/static/js/2.1ac960b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d60ad9773270b0e275",
+    "url": "/apps/lectorem-demo/static/js/main.689856d2.chunk.js"
+  },
+  {
+    "revision": "c90791bea7233a71dcdb",
+    "url": "/apps/lectorem-demo/static/js/runtime-main.1596ab4a.js"
+  }
+]);
--- apps/lectorem-demo/service-worker.js
+++ apps/lectorem-demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5f57958cc72df0793d89654e3aee8de.js"
+  "/apps/lectorem-demo/precache-manifest.f984f7f0e1672c2e3416400d769008f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apps/lectorem-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
